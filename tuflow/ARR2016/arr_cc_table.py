3ab3fe5460f58e77a1e71292b9d8dc84d830b3ed
Ellis Symons <ellis.symons@apac.bmt.org>
2026-05-27 13:38:21 +0100

    2026.0.7 release

--- tuflow/ARR2016/arr_cc_table.py
+++ tuflow/ARR2016/arr_cc_table.py
@@ -10,5 +10,5 @@ class ArrClimateChangeScenTable(QTableWidget):
         self.setItemDelegateForColumn(0, ComboBoxDelegate(self, ['Near-term', 'Medium-term', 'Long-term', '2030',
                                                                  '2040', '2050', '2060', '2070', '2080', '2090', '2100']))
         self.setItemDelegateForColumn(1, ComboBoxDelegate(self, ['SSP1-2.6', 'SSP2-4.5', 'SSP3-7.0', 'SSP5-8.5']))
-        self.setItemDelegateForColumn(2, SpinBoxDelegate(self, 0, 99, 0.1, 1))
-        self.setItemDelegateForColumn(3, SpinBoxDelegate(self, -1, 99, 0.1, 1))
+        self.setItemDelegateForColumn(2, SpinBoxDelegate(self, 0, 99, 0.1, 3))
+        self.setItemDelegateForColumn(3, SpinBoxDelegate(self, -1, 99, 0.1, 3))
